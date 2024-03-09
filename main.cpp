7ba5ae6b6b31bcf2763c085a2d6a7d46f86610f6
dalKonstantin <konser.dal@yandex.com>
2024-03-09 14:48:19 +0300

    prototyping functions

--- main.cpp
+++ main.cpp
@@ -4,8 +4,23 @@
 class Viginere
 {
 	//Encode
+	std::string Encode()
+	{
+		std::string res;
+
+
+		return res;
+	}
 	//Decode
+	std::string Decode()
+	{
+		std::string res;
+
+
+		return res;
+	}
 }
+
 int main()
 {
 
