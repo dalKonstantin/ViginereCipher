4234f792256d7fc9f70ac04a1314fa54b5aa75f6
dalKonstantin <161629422+dalKonstantin@users.noreply.github.com>
2024-03-15 21:49:54 +0300

    final commit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 #include <string>
 
-class Viginere
-{
-	//Encode
-	std::string Encode()
-	{
-		std::string res;
-
-
-		return res;
-	}
-	//Decode
-	std::string Decode()
-	{
-		std::string res;
-
-
-		return res;
-	}
-}
 
 int main()
 {
+	std::string input, input_key;
+	std::string new_key, encrypted, decrypted;
 
+	input = "Konstantin";
+	input_key = "Ikvt";
+
+	for (char& c : input)
+		c = toupper(c);
+	for (char& c : input_key)
+		c = toupper(c);
+
+	//получаем новый ключ
+	for (int i = 0, j = 0; i < input.size(); i++, j++)
+	{
+		if (j == input_key.size())
+			j = 0;
+
+		new_key += input_key[j];
+	}
+
+
+	//кодирование
+	for (int i = 0; i < input.size(); i++)
+	{
+		encrypted += ((input[i] + new_key[i]) % 26) + 'A';
+	}
+
+
+	//декодирование 
+	for (int i = 0; i < input.size(); i++)
+	{
+		decrypted += (((encrypted[i] - new_key[i]) + 26) % 26) + 'A';
+	}
+
+
+	std::cout << encrypted << std::endl << decrypted;
+	
 }
-
-//Жопа
